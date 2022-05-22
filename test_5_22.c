4ba01a1d3c9fac535ba09b72247347ae9fd9241b
liuxuan <104486720+Anduinliu@users.noreply.github.com>
2022-05-22 18:13:28 +0800

    Add files via upload

---
+++ test_5_22.c
@@ -0,0 +1,58 @@
+ #define _CRT_SECURE_NO_WARNINGS  1
+#include"test_5_22.h"
+
+//int main()
+//{
+//	int a, b;
+//	scanf("%d%d", &a, &b);
+//
+//	while (a > b)
+//	{
+//		printf("anduin\n");
+//		a = a - b;
+//	}
+//
+//	return 0;
+//}
+
+//关键字 sizeof：确定一种类型对应在开辟空间的时候 大小
+//int main()
+//{
+//	/*printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));*/
+//
+//	int a = 10;
+//	printf("%d\n", sizeof(a));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof a);//关键字 或 操作符
+//	//printf("%d\n", sizeof int);
+//
+//
+//	return 0;
+//}
+////C中为什么要有类型：本质对内存进行合理化划分，按需索取
+//
+
+//int main()
+//{
+//	int* p = NULL;
+//	int arr[10];
+//	int* test[3];
+//
+//	printf("%d\n", sizeof(p));
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof(test));
+//
+//	return 0;
+//}
+
+//整形存储的本质
+int main()
+{
+	unsigned int b = -10;
+}
